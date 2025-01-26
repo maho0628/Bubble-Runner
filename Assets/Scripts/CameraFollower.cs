77b88e0236125cb94281af822b2eb44905693149
Lee Kent <leedakent123@hotmail.com>
2025-01-26 10:49:40 +0900

    smoothing the camera

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    private void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.x = Remap(transform.parent.position.x, -7.5f, 7.5f, -5f, 5f);
+        transform.position = newPosition;
+    }
+
+    public float Remap(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+}
